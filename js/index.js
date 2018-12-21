bbcd2145f3e20f49af21a4785b048e4dc2043fc7
gad mohamed <gad.mohamed184@yahoo.com>
2018-12-22 01:08:56 +0200

    aside edit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$('#edit-user').on('show.bs.collapse', function () {
+
+    
+    $(".item-list").css("margin-right","250px");
+    
+    
+    
+    
+})
+
+
+
+$('#edit-user').on('hidden.bs.collapse', function () {
+  
+    $(".item-list").css("margin-right","0");
+    
+    
+    
+    
+    
+})
